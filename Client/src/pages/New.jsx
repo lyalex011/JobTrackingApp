89ce99580ae7d2f0240b3be285c8ad08e5d4c1fa
lyalex011 <lytvolex@gmail.com>
2023-08-29 12:08:01 -0600

    form New completed

--- Client/src/pages/New.jsx
+++ Client/src/pages/New.jsx
@@ -8,6 +8,7 @@ import { Select, Option } from "@material-tailwind/react";
 function New({user}) {
     const [defaultDate, setDefaultDate] = useState(''); 
     const [option, setOption] = useState(2);
+    const [showInput, setShowInput] = useState(false);
 
     const optionsState = 2;
 
@@ -23,36 +24,71 @@ function New({user}) {
     const salaryRef = useRef()
     const contactRef = useRef()
     const dateRef = useRef()
+    const checkboxRef = useRef()
+    const intdateRef = useRef()
+    const intTypeRef = useRef()
+    const commentRef = useRef()
+    
 
     const navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault()
         console.log(user)
+        console.log(showInput)
         try {
-            const newJob = {
+          let newJob ={}
+            if(showInput) {
+              newJob = {
                 company: companyRef.current.value,
                 role: positionRef.current.value,
                 url: urlRef.current.value,
                 dateApplied: dateRef.current.value,
                 priority: option,
-                user: user
-                           
+                salary: salaryRef.current.value,
+                contact: contactRef.current.value,
+                comments: commentRef.current.value,
+                yesInterview: showInput,
+                
+                typeInterview: intTypeRef.current.value,
+                dateInterview: intdateRef.current.value,
+                user: user                         
             }
-            console.log(newJob)
+          
+          
+          } else {
+              newJob = {
+                company: companyRef.current.value,
+                role: positionRef.current.value,
+                url: urlRef.current.value,
+                dateApplied: dateRef.current.value,
+                priority: option,
+                salary: salaryRef.current.value,
+                contact: contactRef.current.value,
+                comments: commentRef.current.value,
+                user: user           
+            }
+            
+            }
             await axios.post(`/api/jobs`, newJob, {
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem('token')}`
-                }
-              })
-            navigate(`/`)
-        } catch(err) {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+              }
+            })
+          navigate(`/`)
+            }
+            
+         catch(err) {
             console.log(err.message)
         }
     }
 
-    const handleChange = (event) => {
-      setOption(event.target.value);
+    const handleChange = (e) => {
+      setOption(e.target.value);
+    };
+
+    const handleCheckboxChange = () => {
+      setShowInput(!showInput);
     };
 
 
@@ -83,7 +119,7 @@ function New({user}) {
                     name="company"
                     id="company"
                     autoComplete="company"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={companyRef}
                     required
                     
@@ -104,7 +140,7 @@ function New({user}) {
                     name="Role"
                     id="lastNRoleame"
                     autoComplete="Role"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={positionRef}
                     required
                   />
@@ -124,7 +160,7 @@ function New({user}) {
                     name="url"
                     type="text"
                     autoComplete="url"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={urlRef}
                     
                   />
@@ -144,7 +180,7 @@ function New({user}) {
                     name="date"
                     id="date"
                     autoComplete="date"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={dateRef}
                     value={defaultDate}
                     onChange={(e) => setDefaultDate(e.target.value)}
@@ -155,35 +191,129 @@ function New({user}) {
               </div>
 
               <div className="sm:col-span-3">
-              <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose a priority level</label>
-<select value={option} onChange={handleChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <label htmlFor="priority" className="block mb-2 text-sm font-medium text-gray-900">Choose a priority level</label>
+<select value={option} onChange={handleChange} id="priority" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 ">
   <option value={1}>High</option>
   <option  value={2} >Medium </option>
   <option value={3}>Low</option>
 </select>
               </div>
- 
-                
 
-              <div className="col-span-full">
+
+              <div className="sm:col-span-3">
                 <label
-                  htmlFor="password"
+                  htmlFor="Salary"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Password (At least: 8 characters, 1 number, 1 upper, 1 lower)*
+                  Salary
                 </label>
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="password"
-                    id="password"
-                    autoComplete="password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    name="Salary"
+                    id="Salary"
+                    autoComplete="Salary"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    ref={salaryRef}
                     
                     
                   />
                 </div>
               </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="Contact"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Contact info
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="Contact"
+                    id="lastNRoleame"
+                    autoComplete="Contact"
+                    className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    ref={contactRef}
+                    
+                  />
+                </div>
+              </div>
+                
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="lg-input"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Comments
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    type="textarea"
+                    name="lg-input"
+                    id="lg-input"
+                    autoComplete="lg-input"
+                    className="peer block w-full min-h-[auto] rounded-md border-0 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    ref={commentRef}
+                    
+                  />
+                </div>
+              </div>
+
+          <div className="col-span-full">
+          <label className="inline-flex items-center ">
+            <input
+              type="checkbox"
+              id="showInput"
+              className="form-checkbox rounded-md h-5 w-5 text-sky-700"
+              checked={showInput}
+              onChange={handleCheckboxChange}
+              ref={checkboxRef}
+            />
+            <span className="ml-2 text-gray-700">Interview is scheduled</span>
+          </label>
+          </div>
+          {showInput && (
+            <>
+             <div className="sm:col-span-3">
+             <label
+               htmlFor="date"
+               className="block text-sm font-medium leading-6 text-gray-900"
+             >
+               Date of interview
+             </label>
+             <div className="mt-2">
+               <input
+                 type="date"
+                 name="date"
+                 id="date"
+                 autoComplete="date"
+                 className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                 ref={intdateRef}
+                 
+                 
+                
+                 
+               />
+             </div>
+           </div>
+          <div className="sm:col-span-3">
+          <label htmlFor="priority" className="block mb-2 text-sm font-medium text-gray-900">Choose type of interview</label>
+<select ref={intTypeRef} id="priority" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 ">
+<option value="Phone screen">Phone screen</option>
+<option value="Hiring manager interview">Hiring manager interview</option>
+<option  value="Behavioral interview ">Behavioral interview </option>
+<option value="Second or panel interview ">Second or panel interview</option>
+<option value="Technical interview ">Technical interview </option>
+<option value="Group interview">Group interview</option>
+<option value="Other">Other</option>
+</select>
+          </div>
+          </>
+        )}
+
             </div>
           </div>
         </div>
