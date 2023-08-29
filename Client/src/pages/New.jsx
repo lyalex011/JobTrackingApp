7f0dfc96d4e8795a9f2f0c4d2dee7026f179f29d
lyalex011 <lytvolex@gmail.com>
2023-08-29 12:16:58 -0600

    new form bug fix

--- Client/src/pages/New.jsx
+++ Client/src/pages/New.jsx
@@ -1,99 +1,89 @@
 import { useRef, useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import { Select, Option } from "@material-tailwind/react";
+
+function New({ user }) {
+  const [defaultDate, setDefaultDate] = useState("");
+  const [option, setOption] = useState(2);
+  const [showInput, setShowInput] = useState(false);
 
 
 
-function New({user}) {
-    const [defaultDate, setDefaultDate] = useState(''); 
-    const [option, setOption] = useState(2);
-    const [showInput, setShowInput] = useState(false);
+  useEffect(() => {
+    const today = new Date();
+    const formattedDate = today.toISOString().substr(0, 10);
+    setDefaultDate(formattedDate);
+  }, []);
 
-    const optionsState = 2;
+  const companyRef = useRef();
+  const positionRef = useRef();
+  const urlRef = useRef();
+  const salaryRef = useRef();
+  const contactRef = useRef();
+  const dateRef = useRef();
+  const checkboxRef = useRef();
+  const intdateRef = useRef();
+  const intTypeRef = useRef();
+  const commentRef = useRef();
 
-    useEffect(() => {
-      const today = new Date();
-      const formattedDate = today.toISOString().substr(0, 10);
-      setDefaultDate(formattedDate);
-    }, []);
+  const navigate = useNavigate();
 
-    const companyRef = useRef()
-    const positionRef = useRef()
-    const urlRef = useRef()
-    const salaryRef = useRef()
-    const contactRef = useRef()
-    const dateRef = useRef()
-    const checkboxRef = useRef()
-    const intdateRef = useRef()
-    const intTypeRef = useRef()
-    const commentRef = useRef()
-    
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(user);
+    console.log(showInput);
+    try {
+      let newJob = {};
+      if (showInput) {
+        newJob = {
+          company: companyRef.current.value,
+          role: positionRef.current.value,
+          url: urlRef.current.value,
+          dateApplied: dateRef.current.value,
+          priority: option,
+          salary: salaryRef.current.value,
+          contact: contactRef.current.value,
+          comments: commentRef.current.value,
+          yesInterview: showInput,
 
-    const navigate = useNavigate()
-
-    async function handleSubmit(e) {
-        e.preventDefault()
-        console.log(user)
-        console.log(showInput)
-        try {
-          let newJob ={}
-            if(showInput) {
-              newJob = {
-                company: companyRef.current.value,
-                role: positionRef.current.value,
-                url: urlRef.current.value,
-                dateApplied: dateRef.current.value,
-                priority: option,
-                salary: salaryRef.current.value,
-                contact: contactRef.current.value,
-                comments: commentRef.current.value,
-                yesInterview: showInput,
-                
-                typeInterview: intTypeRef.current.value,
-                dateInterview: intdateRef.current.value,
-                user: user                         
-            }
-          
-          
-          } else {
-              newJob = {
-                company: companyRef.current.value,
-                role: positionRef.current.value,
-                url: urlRef.current.value,
-                dateApplied: dateRef.current.value,
-                priority: option,
-                salary: salaryRef.current.value,
-                contact: contactRef.current.value,
-                comments: commentRef.current.value,
-                user: user           
-            }
-            
-            }
-            await axios.post(`/api/jobs`, newJob, {
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-              }
-            })
-          navigate(`/`)
-            }
-            
-         catch(err) {
-            console.log(err.message)
-        }
+          typeInterview: intTypeRef.current.value,
+          dateInterview: intdateRef.current.value,
+          user: user,
+        };
+      } else {
+        newJob = {
+          company: companyRef.current.value,
+          role: positionRef.current.value,
+          url: urlRef.current.value,
+          dateApplied: dateRef.current.value,
+          priority: option,
+          salary: salaryRef.current.value,
+          contact: contactRef.current.value,
+          comments: commentRef.current.value,
+          user: user,
+        };
+      }
+      await axios.post(`/api/jobs`, newJob, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      navigate(`/`);
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    const handleChange = (e) => {
-      setOption(e.target.value);
-    };
+  const handleChange = (e) => {
+    setOption(e.target.value);
+  };
 
-    const handleCheckboxChange = () => {
-      setShowInput(!showInput);
-    };
+  const handleCheckboxChange = () => {
+    setShowInput(!showInput);
+  };
 
-
-    return ( 
-        <div className="flex justify-center">
+  return (
+    <div className="flex justify-center">
       <form onSubmit={handleSubmit} className="w-3/4 sm:w-1/2 max-w-lg mt-32">
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
@@ -103,7 +93,6 @@ function New({user}) {
             <p className="mt-1 text-sm leading-6 text-gray-600">
               You are only one step from taking control of your job search
             </p>
-            
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-3">
@@ -122,7 +111,6 @@ function New({user}) {
                     className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={companyRef}
                     required
-                    
                   />
                 </div>
               </div>
@@ -162,7 +150,6 @@ function New({user}) {
                     autoComplete="url"
                     className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={urlRef}
-                    
                   />
                 </div>
               </div>
@@ -184,21 +171,28 @@ function New({user}) {
                     ref={dateRef}
                     value={defaultDate}
                     onChange={(e) => setDefaultDate(e.target.value)}
-                   
-                    
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-              <label htmlFor="priority" className="block mb-2 text-sm font-medium text-gray-900">Choose a priority level</label>
-<select value={option} onChange={handleChange} id="priority" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 ">
-  <option value={1}>High</option>
-  <option  value={2} >Medium </option>
-  <option value={3}>Low</option>
-</select>
+                <label
+                  htmlFor="priority"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Choose a priority level
+                </label>
+                <select
+                  value={option}
+                  onChange={handleChange}
+                  id="priority"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 "
+                >
+                  <option value={1}>High</option>
+                  <option value={2}>Medium </option>
+                  <option value={3}>Low</option>
+                </select>
               </div>
-
 
               <div className="sm:col-span-3">
                 <label
@@ -215,8 +209,6 @@ function New({user}) {
                     autoComplete="Salary"
                     className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={salaryRef}
-                    
-                    
                   />
                 </div>
               </div>
@@ -236,11 +228,9 @@ function New({user}) {
                     autoComplete="Contact"
                     className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={contactRef}
-                    
                   />
                 </div>
               </div>
-                
 
               <div className="col-span-full">
                 <label
@@ -257,63 +247,76 @@ function New({user}) {
                     autoComplete="lg-input"
                     className="peer block w-full min-h-[auto] rounded-md border-0 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                     ref={commentRef}
-                    
                   />
                 </div>
               </div>
 
-          <div className="col-span-full">
-          <label className="inline-flex items-center ">
-            <input
-              type="checkbox"
-              id="showInput"
-              className="form-checkbox rounded-md h-5 w-5 text-sky-700"
-              checked={showInput}
-              onChange={handleCheckboxChange}
-              ref={checkboxRef}
-            />
-            <span className="ml-2 text-gray-700">Interview is scheduled</span>
-          </label>
-          </div>
-          {showInput && (
-            <>
-             <div className="sm:col-span-3">
-             <label
-               htmlFor="date"
-               className="block text-sm font-medium leading-6 text-gray-900"
-             >
-               Date of interview
-             </label>
-             <div className="mt-2">
-               <input
-                 type="date"
-                 name="date"
-                 id="date"
-                 autoComplete="date"
-                 className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                 ref={intdateRef}
-                 
-                 
-                
-                 
-               />
-             </div>
-           </div>
-          <div className="sm:col-span-3">
-          <label htmlFor="priority" className="block mb-2 text-sm font-medium text-gray-900">Choose type of interview</label>
-<select ref={intTypeRef} id="priority" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 ">
-<option value="Phone screen">Phone screen</option>
-<option value="Hiring manager interview">Hiring manager interview</option>
-<option  value="Behavioral interview ">Behavioral interview </option>
-<option value="Second or panel interview ">Second or panel interview</option>
-<option value="Technical interview ">Technical interview </option>
-<option value="Group interview">Group interview</option>
-<option value="Other">Other</option>
-</select>
-          </div>
-          </>
-        )}
-
+              <div className="col-span-full">
+                <label className="inline-flex items-center ">
+                  <input
+                    type="checkbox"
+                    id="showInput"
+                    className="form-checkbox rounded-md h-5 w-5 text-sky-700"
+                    checked={showInput}
+                    onChange={handleCheckboxChange}
+                    ref={checkboxRef}
+                  />
+                  <span className="ml-2 text-gray-700">
+                    Interview is scheduled
+                  </span>
+                </label>
+              </div>
+              {showInput && (
+                <>
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="date"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Date of interview
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="date"
+                        name="date"
+                        id="date"
+                        autoComplete="date"
+                        className="block w-full rounded-md border-0 py-1.5 leading-[1.6] outline-none transition-all duration-200 ease-linear text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                        ref={intdateRef}
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="priority"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Choose type of interview
+                    </label>
+                    <select
+                      ref={intTypeRef}
+                      id="priority"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 "
+                    >
+                      <option value="Phone screen">Phone screen</option>
+                      <option value="Hiring manager interview">
+                        Hiring manager interview
+                      </option>
+                      <option value="Behavioral interview">
+                        Behavioral interview{" "}
+                      </option>
+                      <option value="Second or panel interview">
+                        Second or panel interview
+                      </option>
+                      <option value="Technical interview">
+                        Technical interview{" "}
+                      </option>
+                      <option value="Group interview">Group interview</option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
@@ -336,7 +339,7 @@ function New({user}) {
         </div>
       </form>
     </div>
-     );
+  );
 }
 
 export default New;
